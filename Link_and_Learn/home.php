ba37f18ceb15e3b61a26c66a477b3f012ce6ca50
Zackjw70 <zjwyeth@email.neit.edu>
2024-01-29 13:14:06 -0500

    Button

--- Link_and_Learn/home.php
+++ Link_and_Learn/home.php
@@ -5,8 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Link-Up and Learn</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link href="Stylesheets/style.css" rel="stylesheet">
+    
 </head>
 <body>
+    <button class="custom-btn btn-14">Read More</button>
     
 </body>
 </html>
