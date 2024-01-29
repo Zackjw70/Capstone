5d1195853c4ad935735bb0e768dbfda4d211e339
Zackjw70 <zjwyeth@email.neit.edu>
2024-01-29 13:57:36 -0500

    Page finished - Boom

--- Link_and_Learn/Logout.php
+++ Link_and_Learn/Logout.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Logout</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link href="Stylesheets/style.css" rel="stylesheet">
 </head>
 <body>
+    
+    <button class="custom-btn btn-14">Read More</button>
     
 </body>
 </html>
--- Link_and_Learn/home.php
+++ Link_and_Learn/home.php
@@ -7,10 +7,28 @@
     <title>Link-Up and Learn</title>
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
     <link href="Stylesheets/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
 </head>
 <body>
-    <button class="custom-btn btn-14">Read More</button>
+    <nav class="navbar navbar-expand-lg">
+        <a href="home.php">
+            <img src="images/Link-up_and_Learn_Logo.png" alt="Home">
+        </a>
+        <h1>Link up and Learn</h1>
+        <a href="Login.php">
+            <button class="custom-btn btn-14">
+                Login
+            </button>
+        </a>
+    </nav>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+    
     
 </body>
 </html>
