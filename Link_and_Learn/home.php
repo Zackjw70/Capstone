9500afd405fec0ab8bc4967b07dce19dccdbc15e
Zackjw70 <zjwyeth@email.neit.edu>
2024-02-02 12:41:27 -0500

    i hate

--- Link_and_Learn/home.php
+++ Link_and_Learn/home.php
@@ -5,23 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Link-Up and Learn</title>
+    <link href="XXX.css?v=1.0" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
-    <link href="Stylesheets/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="Stylesheets/style.css" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+
     
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg">
-        <a href="home.php">
-            <img src="images/Link-up_and_Learn_Logo.png" alt="Home">
-        </a>
-        <h1>Link up and Learn</h1>
-        <a href="Login.php">
-            <button class="custom-btn btn-14">
-                Login
-            </button>
-        </a>
-    </nav>
+    <div>
+        <div class="row" style="width:100%;">
+            <div class="col-1 text-start">
+                <a href="home.php">
+                    <img src="images/Link-up_and_Learn_Logo.png" alt="Home">
+                </a>
+            </div>
+            <div class="col-10 text-center">
+                <h1 class="titleLink">Link up and Learn</h1>
+            </div>
+            <div class="col-1 text-center">
+                <a href="Login.php">
+                <button class="custom-btn btn-14">
+                    Login
+                </button>
+                </a>
+            </div>
+        </div>
+        
+        
+        
+        
+    </div>
     <div>
 
     </div>
