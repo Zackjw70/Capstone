93e7670faab533391091cb27584522703770cda6
Strider-Day <ajrockwell@email.neit.edu>
2024-02-02 13:44:50 -0500

    Update modelLink.php
    uhh oops

--- Link_and_Learn/DBModel/modelLink.php
+++ Link_and_Learn/DBModel/modelLink.php
@@ -15,6 +15,43 @@ function getUsers(){
     return ($results);
 }
 
+function searchUsers($username){
+    global $db;
+    $binds = array();
+
+    $sql = "SELECT * FROM users WHERE 0=0";
+    if ($username != "") {
+        $sql .= " AND username Like :username";
+        $binds['username'] = '%'.$username.'%';
+    }
+
+    $results = array();
+    $stmt = $db->prepare($sql);
+    if ($stmt->execute($binds) && $stmt->rowCount() > 0){
+        $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    }
+
+    return ($results);
+}
+
+function addUsers ($username, $logpassword, $perm){
+    global $db;
+    $result = "";
+    $stmt = $db->prepare("INSERT INTO users set username = :un, logpassword = :lp, perm = :pm");
+
+    $binds = array(
+        ":un" => $username,
+        ":lp" => $logpassword,
+        ":pm" => $perm
+    );
+
+    if ($stmt->execute($binds) && $stmt->rowCount() > 0){
+        $result = 'Data Added';
+    }
+
+    return ($result);
+}
+
 
 //All functions for ChangeLog Table
 
@@ -24,6 +61,35 @@ function getChangeLog(){
     $sqlstring = $db ->prepare("SELECT changeid, infoid, imageid, changetime, username, successful FROM changelog ORDER BY chagetime");
     if ($sqlstring -> execute() && $sqlstring ->rowCount() > 0){
         $results = $sqlstring -> fetchALL(PDO::FETCH_ASSOC);
+    }
+
+    return ($results);
+}
+
+function searchChangeLog($username, $infoid, $imageid){
+    global $db;
+    $binds = array();
+
+    $sql = "SELECT * FROM changelog WHERE 0=0";
+    if ($username != "") {
+        $sql .= " AND username Like :username";
+        $binds['username'] = '%'.$username.'%';
+    }
+
+    if ($infoid != "") {
+        $sql .= " AND infoid Like :infoid";
+        $binds['info'] = '%'.$infoid.'%';
+    }
+
+    if ($imageid != "") {
+        $sql .= " AND imageid Like :imageid";
+        $binds['imageid'] = '%'.$imageid.'%';
+    }
+
+    $results = array();
+    $stmt = $db->prepare($sql);
+    if ($stmt->execute($binds) && $stmt->rowCount() > 0){
+        $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
     }
 
     return ($results);
@@ -47,7 +113,7 @@ function searchReviews($client){
     $binds = array();
 
     $sql = "SELECT * FROM reviews WHERE 0=0";
-    if ($first != "") {
+    if ($client != "") {
         $sql .= " AND client Like :client";
         $binds['client'] = '%'.$client.'%';
     }
@@ -73,4 +139,25 @@ function getLoginAttempts(){
 
     return ($results);
 }
+
+function searchLoginAttempts($username){
+    global $db;
+    $binds = array();
+
+    $sql = "SELECT * FROM loginattempts WHERE 0=0";
+    if ($username != "") {
+        $sql .= " AND username Like :username";
+        $binds['username'] = '%'.$username.'%';
+    }
+
+    $results = array();
+    $stmt = $db->prepare($sql);
+    if ($stmt->execute($binds) && $stmt->rowCount() > 0){
+        $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    }
+
+    return ($results);
+}
+
+?>
 
