4edd0ef6395c65a585ddf204087d02c33563b4b3
Zackjw70 <zjwyeth@email.neit.edu>
2024-02-04 18:18:04 -0500

    Header

--- Link_and_Learn/home.php
+++ Link_and_Learn/home.php
@@ -16,28 +16,49 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="Stylesheets/style.css" type="text/css">
     <style>
+        body{
+            background-color:#EAD064;
+        }
         h1{
             font-family: "Architects Daughter", cursive;
             font-weight: 400;
             font-style: normal;
             height: 50px;
             color:#B93836;
-}
+            font-size:60px;
+            margin-left:auto;margin-right:auto;
+            margin-top:50px;
+            
+        }
+        .loginbtn{
+            margin-top:80px;
+            width: 200px;
+            height: 60px;
+            font-size:26px;
+
+        }
+        .headerrow{
+            background-color:#EABC64;
+        }
+        .logobtn{
+            opacity: 50%;
+        }
+
     </style>
     
 </head>
 <body>
     <div>
-        <div class="row" style="width:100%;">
-            <div class="col-1 text-start">
+        <div class="row headerrow" style="width:101%;">
+            <div class="col-2 text-start">
                 <a href="home.php">
-                    <img src="images/Link-up_and_Learn_Logo.png" alt="Home">
+                    <img src="images/Link-up_and_Learn_Logo.png" alt="Home" class="logobtn">
                 </a>
             </div>
-            <div class="col-10 text-center">
-                <h1 id="bootstrap-overrides">Link up and Learn</h1>
+            <div class="col-8 text-center">
+                <h1>Link up and Learn</h1>
             </div>
-            <div class="col-1 text-center">
+            <div class="col-2 text-center">
                 <?php if(isset($_SESSION['user'])): ?>
                     <h3><?= $_SESSION['user']; ?></h3>
                     <a href="Logout.php">
@@ -47,7 +68,7 @@
                 </a>
                 <?php else: ?>
                 <a href="Login.php">
-                <button class="custom-btn btn-14">
+                <button class="custom-btn btn-14 loginbtn">
                     Login
                 </button>
                 </a>
